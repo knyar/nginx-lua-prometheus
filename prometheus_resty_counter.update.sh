d8cab0d0d87774e503a853101a9cd30e43b6f6b9
Anton Tolchanov <commits@knyar.net>
2020-04-22 08:33:08 +0000

    Clarify the purpose of prometheus_resty_counter.update.sh (fix #80)

--- prometheus_resty_counter.update.sh
+++ prometheus_resty_counter.update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# This script updates the lua-resty-counter [1] included with this library
-# for convenience.
+# This script downloads the lua-resty-counter [1] included with this library
+# for convenience. It's here to make it easier to update the "vendored" lua
+# file in this repo. As a user, you should not need to use this script.
 # [1] https://github.com/Kong/lua-resty-counter
 
 set -exu
